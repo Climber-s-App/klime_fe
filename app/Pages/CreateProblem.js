9182ad185fba71144f956d3e27af068efee0c385
Jade Shi <ziyushi1997@gmail.com>
2023-08-29 14:53:42 -0700

    Works!!!! inital position bug

--- app/Pages/CreateProblem.js
+++ app/Pages/CreateProblem.js
@@ -21,6 +21,7 @@ const CreateProblem = ({ vectorColor }) => {
   useEffect(() => {
     console.log("newVectors: ", newVectors);
   }, [newVectors]);
+    
 
   const onSingleTap = (event) => {
     // alert("hi");
@@ -28,13 +29,18 @@ const CreateProblem = ({ vectorColor }) => {
       // alert("tap!");
       console.log("here");
       const { x, y } = event.nativeEvent;
-      console.log("native event: ", event.nativeEvent);
+      console.log("Tap native event: ", event.nativeEvent);
       const addVector = {
         color: `#${vectorColor}`,
         x: x - 15,
         y: y - 15,
+        initialPositionX: x - 15,
+        initialPositionY: y - 15,
         id: uuid(),
+        animatedStartingPoint: new Animated.ValueXY({x, y})
       };
+
+      // console.log('animated starting point: ', addVector.animatedStartingPoint)
 
       setNewVectors((prevVectors) => [...prevVectors, addVector]);
       console.log(newVectors);
@@ -44,12 +50,21 @@ const CreateProblem = ({ vectorColor }) => {
 
   onPanGestureEvent = (event, vector) => {
     const { translationX, translationY } = event.nativeEvent;
+    console.log('native event on pan guesture: ', event.nativeEvent)
+    // console.log('pan anim start: ', vector.animatedStartingPoint)
+    console.log('translationX, translationY', translationX, translationY)
+    // const startX = vector.animatedStartingPoint.x;
+    // const startY = vector.animatedStartingPoint.y;
 
+    // console.log('startX, StartY: ', startX, startY)
     const updatedVector = {
       ...vector,
-      x: vector.x + translationX,
-      y: vector.y + translationY,
+      x: vector.initialPositionX + translationX,
+      y: vector.initialPositionY + translationY,
     };
+
+    // console.log('updatedVector: ', updatedVector.x, updatedVector.y)
+    // console.log('__________________')
 
     const updatedVectors = newVectors.map((v) =>
       v.id === updatedVector.id ? updatedVector : v
@@ -58,7 +73,7 @@ const CreateProblem = ({ vectorColor }) => {
     setNewVectors(updatedVectors);
   };
 
-  const savedVectors = newVectors.map((vector) => {
+  const savedVectors = newVectors.map((vector, index) => {
     const { color, x, y, id } = vector;
     const vectorStyle = {
       transform: [{ translateX: x }, { translateY: y }],
@@ -69,6 +84,17 @@ const CreateProblem = ({ vectorColor }) => {
       <PanGestureHandler
         key={id}
         onGestureEvent={(event) => onPanGestureEvent(event, vector)}
+        onPanHandlerStateChange={(event) => {
+          if (event.nativeEvent.state === State.BEGAN) {
+            const { x, y } = vector;
+            const updatedVector = {
+              ...vector,
+              initialPositionX: x,
+              initialPositionY: y
+            };
+            setNewVectors(newVectors.splice(index, 1, updatedVector));
+          }
+        }}
       >
         <Animated.View key={id} style={vectorStyle}>
           <AddVectors vectorColor={color} />
