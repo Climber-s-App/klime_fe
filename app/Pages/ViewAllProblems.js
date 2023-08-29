d4d04e48c90cbef4c756c2e0878507b5c8f07d0e
Hollis Vohr <hollis.vohr@gmail.com>
2023-08-29 00:04:48 -0600

    fix: fetch call in view all problems to set updated state

--- app/Pages/ViewAllProblems.js
+++ app/Pages/ViewAllProblems.js
@@ -1,14 +1,16 @@
 import { Text, StyleSheet } from 'react-native';
 import {getAllProblems} from '../Components/apiCalls'
+import { useState, useEffect } from 'react';
 
 export default function ViewAllProblems({route}) {
+  const [problems, setSavedProblems] = useState([])
   const { id } = route.params
 
   useEffect(() => {
     (async () => {
       const data = await getAllProblems(id);
       const modifiedData = data.data.map((data) => ({ id: data.id, ...data.attributes }))
-      setSavedWalls(modifiedData)
+      setSavedProblems(modifiedData)
     })();
   }, [])
 
