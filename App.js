baa49c57463b45e0609f455d82820cbf6eddd5ce
Jade Shi <ziyushi1997@gmail.com>
2023-08-29 07:50:21 -0700

    Add style to GestureHandlerRootView

--- App.js
+++ App.js
@@ -34,7 +34,7 @@ export default function App() {
   }
 
   return (
-    <GestureHandlerRootView>
+    <GestureHandlerRootView style={{ ...styles.container, ...styles.androidSafeArea }}>
     <SafeAreaView style={{ ...styles.container, ...styles.androidSafeArea }}> 
         <StatusBar/>
         <View style={styles.contentContainer}>
