e39dd3421f3754fdd7fb896286ed70c3a5ee71ee
Hollis Vohr <hollis.vohr@gmail.com>
2023-08-28 21:30:29 -0600

    feat: add new page for viewing problems

---
+++ app/Pages/ViewAllProblems.js
@@ -0,0 +1,7 @@
+import { Text, View, Image, ScrollView, StyleSheet } from 'react-native';
+
+export default function ViewAllProblems() {
+  return (
+    <Text>Here are all wall specific problems</Text>
+  )
+}
