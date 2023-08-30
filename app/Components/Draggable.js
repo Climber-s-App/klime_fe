c212790bd66cab43d0832589641e3baac131d486
Jade Shi <ziyushi1997@gmail.com>
2023-08-30 16:10:11 -0700

    Add handleFinalize

--- app/Components/Draggable.js
+++ app/Components/Draggable.js
@@ -9,32 +9,42 @@ export default function Draggable({
   newVectors,
   setNewVectors,
 }) {
-  console.log('vectorStyle: ', vectorStyle)
+  const updateNewVectors = (updatedVector) => {
+    const updatedVectors = newVectors.map((v) =>
+      v.id === updatedVector.id ? updatedVector : v
+    );
+
+    setNewVectors(updatedVectors);
+  };
+
   const handleChange = (event) => {
     const { translationX, translationY } = event;
-   
     const updatedVector = {
       ...vector,
       x: vector.initialX + translationX,
       y: vector.initialY + translationY,
     };
-
-    const updatedVectors = newVectors.map((v) =>
-      v.id === updatedVector.id ? updatedVector : v
-    );
-    setNewVectors(updatedVectors);
-
+    updateNewVectors(updatedVector);
   };
 
-  const handleFinalize = (event) => {};
+  const handleFinalize = (event) => {
+    const { x, y } = vector;
+    const updatedVector = {
+      ...vector,
+      initialX: x,
+      initialY: y,
+    };
+    updateNewVectors(updatedVector);
+  };
 
   const pan = Gesture.Pan()
     .onBegin((event) => {})
     .onChange((event) => {
       console.log(event);
-      handleChange(event)
+      handleChange(event);
     })
     .onFinalize((event) => {
+      handleFinalize(event)
       alert("end");
     });
 
