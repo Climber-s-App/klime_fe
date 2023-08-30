deffd75ab11df5e94c8ec4300fc4b1e7dfd4f061
Jade Shi <ziyushi1997@gmail.com>
2023-08-30 14:56:48 -0700

    Remove Pressable

--- App.js
+++ App.js
@@ -1,51 +1,60 @@
-import { useState } from 'react';
-import "react-native-gesture-handler"
-import { StyleSheet, SafeAreaView, View, StatusBar } from 'react-native';
-import ViewProblem from './app/Pages/ViewProblem';
-import CreateProblem from './app/Pages/CreateProblem';
-import Home from './app/Pages/Home';
-import MenuBar from './app/Components/MenuBar';
-import RouteContext from './app/Components/RouteContext';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import ViewAllProblems from '../klime_fe/app/Pages/ViewAllProblems'
-
+import { useState } from "react";
+import "react-native-gesture-handler";
+import { StyleSheet, SafeAreaView, View, StatusBar } from "react-native";
+import ViewProblem from "./app/Pages/ViewProblem";
+import CreateProblem from "./app/Pages/CreateProblem";
+import Home from "./app/Pages/Home";
+import MenuBar from "./app/Components/MenuBar";
+import RouteContext from "./app/Components/RouteContext";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import ViewAllProblems from "../klime_fe/app/Pages/ViewAllProblems";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
-  const Stack = createNativeStackNavigator()
-  const [currentRoute, setCurrentRoute] = useState('Home')
-  const [vectorColor, setVectorColor] = useState('60FF46')
+  const Stack = createNativeStackNavigator();
+  const [currentRoute, setCurrentRoute] = useState("Home");
+  const [vectorColor, setVectorColor] = useState("60FF46");
 
   const handleVectorColor = (color) => {
     switch (color) {
-      case '60FF46':
-        setVectorColor('16e8f7');
+      case "60FF46":
+        setVectorColor("16e8f7");
         break;
-      case '16e8f7':
-        setVectorColor('f72556');
+      case "16e8f7":
+        setVectorColor("f72556");
         break;
-      case 'f72556':
-        setVectorColor('60FF46');
+      case "f72556":
+        setVectorColor("60FF46");
         break;
     }
-  }  
+  };
 
   return (
-    <RouteContext.Provider value={{currentRoute, setCurrentRoute, vectorColor}}>
-      <NavigationContainer style={{ ...styles.container, ...styles.androidSafeArea }}>
+    <RouteContext.Provider
+      value={{ currentRoute, setCurrentRoute, vectorColor }}
+    >
+      <NavigationContainer
+        style={{ ...styles.container, ...styles.androidSafeArea }}
+      >
         <Stack.Navigator>
-            <Stack.Screen name='Home' component={Home} />
-            <Stack.Screen name='View All Problems' component={ViewAllProblems} />
-            <Stack.Screen name='View Problem' component={ViewProblem} />
-            <Stack.Screen name='Create Problem' component={CreateProblem} />
-          </Stack.Navigator>
-        <SafeAreaView>
-          <StatusBar />
-          <View style={styles.contentContainer} />
-          <View style={styles.menuContainer}>
-            <MenuBar vectorColor={vectorColor} handleVectorColor={handleVectorColor} />
-          </View>
-        </SafeAreaView>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="View All Problems" component={ViewAllProblems} />
+          <Stack.Screen name="View Problem" component={ViewProblem} />
+          <Stack.Screen name="Create Problem" component={CreateProblem} />
+        </Stack.Navigator>
+        <GestureHandlerRootView>
+          <SafeAreaView>
+            <StatusBar />
+            <View style={styles.contentContainer} />
+            <View style={styles.menuContainer}>
+              <MenuBar
+                vectorColor={vectorColor}
+                handleVectorColor={handleVectorColor}
+              />
+            </View>
+          </SafeAreaView>
+        </GestureHandlerRootView>
       </NavigationContainer>
     </RouteContext.Provider>
   );
@@ -54,22 +63,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: '100%',
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    width: "100%",
+    backgroundColor: "#fff",
+    alignItems: "center",
   },
   contentContainer: {
     flex: 1,
-    width: '100%',
-    alignItems: 'center',
+    width: "100%",
+    alignItems: "center",
   },
   androidSafeArea: {
     flex: 1,
     backgroundColor: "white",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
   menuContainer: {
     bottom: 0,
-    width: '100%',
-  }
+    width: "100%",
+  },
 });
